57a9e0058024d738e8885d0134b4f041a101206d
Konstantin Churakaev <konst.churakaev@gmail.com>
2024-04-16 12:59:53 +0500

    Initial commit

---
+++ src/main/java/org/example/Quadratic.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.ArrayList;
+
+import static java.lang.Double.NaN;
+
+public class Quadratic {
+
+    public ArrayList<Double> solve(Object A, Object B, Object C, Object D) {
+
+        double a= checkDouble(A);
+        double b = checkDouble(B);
+        double c = checkDouble(C);
+        double delta = checkDouble(D);
+
+
+
+        ArrayList<Double> solve = new ArrayList<>();
+        double d = b * b - 4 * a * c;
+
+        if (Math.abs(a) < delta) {
+            throw new NullPointerException("String can not be empty!");
+        }
+        double x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+        double x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
+        if (x1 != NaN) {
+            solve.add(x1);
+            if (Math.abs(x1 - x2) > delta) {
+                solve.add(x2);
+            }
+        }
+        return solve;
+    }
+
+    private double checkDouble(Object o){
+        if (o instanceof Double){
+            return (double) o;
+        }else {
+            throw new ClassCastException("invalid data type");
+        }
+    }
+
+}
---
+++ src/test/java/org/example/QuadraticTest.java
@@ -0,0 +1,92 @@
+package org.example;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class QuadraticTest {
+    Quadratic solver = new Quadratic();
+    @Test
+    public void testNoRoots() {
+
+        double a = 1;
+        double b = 0;
+        double c = 1;
+        double delta = 0.000001;
+        ArrayList<Double> solve = solver.solve(a, b, c, delta);
+        assertNotNull(solve);
+        assertFalse(solve.isEmpty());
+
+    }
+    @Test
+    public void testOneRoots() {
+
+        double a = 1;
+        double b = 2;
+        double c = 1;
+        double delta = 0.000001;
+        ArrayList<Double> solve = solver.solve(a, b, c, delta);
+        assertNotNull(solve);
+        assertFalse(solve.size()!=1);
+        double x = solve.get(0);
+        assertEquals(calculated(a, b, c, x), 0.0, delta);
+    }
+
+    @Test
+    public void testTwoRoots(){
+        double a = 1;
+        double b = 0;
+        double c = -1;
+        double delta = 0.000001;
+        ArrayList<Double> solve = solver.solve(a, b, c, delta);
+        assertNotNull(solve);
+        assertFalse(solve.size()!=2);
+        double x1 = solve.get(0);
+        double x2 = solve.get(1);
+        assertEquals(calculated(a, b, c, x1), 0.0, delta);
+        assertEquals(calculated(a, b, c, x2), 0.0, delta);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullA() {
+        double a = 0.0000005;
+        double b = 0;
+        double c = -1;
+        double delta = 0.000001;
+
+        if (Math.abs(a) < delta) {
+            throw new NullPointerException("A can not be empty!");
+        }
+    }
+
+    @Test
+    public void testNotNumeric(){
+        String a = "0.0000005";
+        boolean b = false;
+        char c = 10;
+        double delta = 0.000001;
+
+        ArrayList<Double> solve = solver.solve(a, b, c, delta);
+    }
+
+    @Test
+    public void testSolveInvalidInput() {
+
+        String a = "0.0000005";
+        boolean b = false;
+        char c = 10;
+        double delta = 0.000001;
+
+        assertThrows(ClassCastException.class, () -> {
+            solver.solve(a, b, c, delta);
+        });
+    }
+
+    private double calculated(double a, double b, double c, double x){
+        return a * x * x + b * x + c;
+    }
+
+}
